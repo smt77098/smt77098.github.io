3731d41856cd6f8bfa2846d0914c3c08f755945e
stimlin <smt77098@hawkmail.hacc.edu>
2023-12-09 10:41:59 -0500

    project 4 edits

---
+++ projects/project-4/script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+     // Get today's date
+     const d = new Date();
+  let day = d.getDay(); // This will give us a number from 0 to 6
+
+  // Find the div by ID and manipulate the display style
+  document.getElementById(`day${day}`).style.display = 'block';
+//
+
