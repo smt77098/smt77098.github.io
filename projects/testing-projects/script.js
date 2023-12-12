b2edf2d22c75a4caad6664aa123d17e102cc83bc
stimlin <smt77098@hawkmail.hacc.edu>
2023-12-11 19:55:31 -0500

    project 4 switch from test folder to correct folder

--- projects/testing-projects/script.js
+++ projects/testing-projects/script.js
@@ -1,11 +1,12 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
      // Get today's date
      const d = new Date();
